09d0b1e99a4fa401723b73c32cad8a1c86446cc2
Eric <ericta1911@gmail.com>
2022-09-20 22:23:20 +0700

    setState all elemnt in page but not ready send data for server

--- pages/admin/manage-product.js
+++ pages/admin/manage-product.js
@@ -1,39 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import LayoutAmin from "../../layout/LayoutAdmin";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 import TextField from "@mui/material/TextField";
 import { ArrowUpTrayIcon } from "@heroicons/react/24/outline";
-import { TextareaAutosize } from "@mui/material";
+import { OutlinedInput, TextareaAutosize } from "@mui/material";
 import { getAllCategory } from "../../services/productService";
+import CommonUtils from "../../utility/CommonUtils";
+import { Category } from "@mui/icons-material";
 
-const currencies = [
-  {
-    value: "USD",
-    label: "$",
-  },
-  {
-    value: "EUR",
-    label: "€",
-  },
-  {
-    value: "BTC",
-    label: "฿",
-  },
-  {
-    value: "JPY",
-    label: "¥",
-  },
-];
 export default function ManageProduct({ data }) {
-  console.log("check data manage product", data);
-  useEffect(() => {}, []);
-  const [currency, setCurrency] = React.useState("EUR");
+  const dataUrlImg = "";
+  const [title, setTitle] = React.useState("");
+  const [price, setPrice] = React.useState("");
+  const [discount, setDiscount] = React.useState("");
+  const [urlImg, setUrlImg] = React.useState("");
+  const [description, setDescription] = React.useState("");
 
-  const handleChange = (event) => {
-    setCurrency(event.target.value);
+  const arrCategory = data.data;
+  const [currency, setCurrency] = React.useState("");
+
+  const handleChange = (e) => {
+    console.log(e.target);
+    setCurrency(e.target.value);
   };
+
+  const handleOnchangeImage = async (e) => {
+    const data = e.target.files;
+    const file = data[0];
+    if (file) {
+      let base64 = await CommonUtils.getBase64(file);
+
+      const URLFile = URL.createObjectURL(file);
+      setUrlImg(base64);
+    }
+  };
+  const handleClickButton = () => {
+    let data = {
+      categoryId: currency,
+      title: title,
+      price: price,
+      discount: discount,
+      thumbnail: dataUrlImg,
+      description: description,
+    };
+    console.log("check data send server:", data);
+  };
+
+  console.log("check change category:", currency);
   return (
     <>
       <LayoutAmin />
@@ -48,52 +68,65 @@ export default function ManageProduct({ data }) {
           autoComplete="off"
         >
           <div>
-            <TextField
-              id="outlined-select-currency-native"
-              select
-              label="CategoryId"
-              value={currency}
-              onChange={handleChange}
-              SelectProps={{
-                native: true,
-              }}
-              helperText="Please select your category"
-            >
-              {currencies.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
+            <select onChange={(e) => handleChange(e)}>
+              {arrCategory.map((category, index) => (
+                <option key={index} value={category._id}>
+                  {category.name}
                 </option>
               ))}
-            </TextField>
-            <TextField required id="outlined-required" label="Title" />
-            <TextField required id="outlined-required" label="Price" />
-            <TextField required id="outlined-required" label="Discount" />
-            <div className="flex justify-between">
-              <div className="w-[40%] ">
-                <label htmlFor="img-input">
-                  Choose picture
-                  <input
-                    className="border-[1px] border-gray-400"
-                    id="img-input"
-                    type="file"
-                    class="file:border file:border-solid"
-                  />
-                </label>
-              </div>
-              <div className="flex flex-col w-[60%] ">
-                <label> Description</label>
-                <TextareaAutosize
-                  className="border-[1px] border-gray-500 outline-hidden"
-                  aria-label="empty textarea"
-                  placeholder=""
-                  minRows={1}
-                  style={{ width: "100%" }}
+            </select>
+          </div>
+          <TextField
+            required
+            id="outlined-required"
+            label="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Discount"
+            value={discount}
+            onChange={(e) => setDiscount(e.target.value)}
+          />
+          <div className="flex justify-between">
+            <div className="w-[40%] ">
+              <label htmlFor="img-input">
+                Choose picture
+                <input
+                  className="border-[1px] border-gray-400"
+                  id="img-input"
+                  type="file"
+                  onChange={(e) => handleOnchangeImage(e)}
                 />
-              </div>
+              </label>
+            </div>
+            <div className="flex flex-col w-[60%] ">
+              <label> Description</label>
+              <TextareaAutosize
+                className="border-[1px] border-gray-500 outline-hidden"
+                aria-label="empty textarea"
+                placeholder=""
+                minRows={1}
+                style={{ width: "100%" }}
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
             </div>
           </div>
         </Box>
-        <Button className="bg-green-400 text-black hover:bg-green-700 mt-12 px-7">
+        <Button
+          className="bg-green-400 text-black hover:bg-green-700 mt-12 px-7"
+          onClick={() => handleClickButton()}
+        >
           Create
         </Button>
       </div>
